c83f14e881442a0947acc9fcb537ea9bb93bd42f
QChan <>
2025-02-19 16:14:26 +0300

    Add better views for comments

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,22 +1,20 @@
 from django.urls import path, include
-from rest_framework.routers import SimpleRouter
-from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework.routers import DefaultRouter
 
 from .views import PostViewSet, GroupViewSet, FollowViewSet, CommentViewSet
 
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register("posts", PostViewSet)
 router.register("follow", FollowViewSet)
 router.register("groups", GroupViewSet)
-
-comments_router = NestedDefaultRouter(router, "posts", lookup="post")
-comments_router.register("comments", CommentViewSet, basename="post-comments")
+router.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comments'
+)
 
 
 urlpatterns = [
     path("v1/", include("djoser.urls")),
     path("v1/", include("djoser.urls.jwt")),
     path("v1/", include(router.urls)),
-    path("v1/", include(comments_router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
-from rest_framework import status, viewsets, filters, mixins
-from rest_framework.exceptions import PermissionDenied
+from rest_framework import viewsets, filters, mixins
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
@@ -12,20 +10,14 @@ from .serializers import (
     CommentSerializer,
     FollowSerializer,
 )
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     pagination_class = LimitOffsetPagination
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-
-    def get_object(self):
-        obj = super().get_object()
-        if self.request.method not in ["GET", "HEAD", "OPTIONS"]:
-            if obj.author != self.request.user:
-                raise PermissionDenied("У вас недостаточно прав")
-        return obj
+    permission_classes = (IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -54,30 +46,13 @@ class FollowViewSet(
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly)
 
     def get_queryset(self):
-        post_id = self.kwargs.get("post_pk")
-        return Comment.objects.filter(post_id=post_id)
+        post_id = self.kwargs.get('post_id')
+        new_queryset = Comment.objects.filter(post=post_id)
+        return new_queryset
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, id=self.kwargs.get("post_pk"))
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
         serializer.save(author=self.request.user, post=post)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.author != request.user:
-            return Response(
-                {"detail": "Вы не можете редактировать этот комментарий."},
-                status=status.HTTP_403_FORBIDDEN,
-            )
-        return super().update(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if instance.author != request.user:
-            return Response(
-                {"detail": "Вы не можете удалить этот комментарий."},
-                status=status.HTTP_403_FORBIDDEN,
-            )
-        return super().destroy(request, *args, **kwargs)
